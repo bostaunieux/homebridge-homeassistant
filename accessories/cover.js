7141d79243999484bb0f0f02706f647e1f20d41f
Damien Angelos <bostaunieux@gmail.com>
2018-08-10 17:02:11 -0400

    Add support for horizontal blinds cover type (#1)
    * expose cover availability
    * add horizontal blind cover type
    * update readme

--- accessories/cover.js
+++ accessories/cover.js
@@ -45,7 +45,12 @@ class HomeAssistantCover {
   getState(callback) {
     this.client.fetchState(this.entity_id, (data) => {
       if (data) {
-        callback(null, this.transformData(data));
+        const transformedData = this.transformData(data);
+        if (transformedData !== null) {
+          callback(null, this.transformData(data));
+        } else {
+          callback(communicationError);
+        }
       } else {
         callback(communicationError);
       }
@@ -103,6 +108,10 @@ class HomeAssistantGarageDoor extends HomeAssistantCover {
   }
 
   transformData(data) {
+    if (data.state === 'unavailable') {
+      return null;
+    }
+
     return data.state === 'closed' ? this.stateCharacteristic.CLOSED : this.stateCharacteristic.OPEN;
   }
 
@@ -130,7 +139,7 @@ class HomeAssistantRollershutter extends HomeAssistantCover {
   }
 
   transformData(data) {
-    return (data && data.attributes) ? data.attributes.current_position : null;
+    return (data && data.attributes && data.state !== 'unavailable') ? data.attributes.current_position : null;
   }
 
   setTargetState(position, callback, context) {
@@ -156,9 +165,49 @@ class HomeAssistantRollershutter extends HomeAssistantCover {
   }
 }
 
+class HomeAssistantHorizontalBlind extends HomeAssistantRollershutter {
+  constructor(log, data, client, firmware) {
+    super(log, data, client, firmware);
+    if (data.attributes && data.attributes.homebridge_model) {
+      this.model = String(data.attributes.homebridge_model);
+    } else {
+      this.model = 'Horizontal Blind';
+    }
+    this.service = new Service.WindowCovering();
+    this.stateCharacteristic = Characteristic.CurrentPosition;
+    this.targetCharacteristic = Characteristic.TargetPosition;
+  }
+
+  transformData(data) {
+    return (data && data.attributes && data.state !== 'unavailable') ? data.attributes.current_tilt_position : null;
+  }
+
+  setTargetState(position, callback, context) {
+    if (context === 'internal') {
+      callback();
+      return;
+    }
+
+    const payload = {
+      entity_id: this.entity_id,
+      tilt_position: position,
+    };
+
+    this.log(`Setting the state of the ${this.name} to ${payload.position}`);
+
+    this.client.callService(this.domain, 'set_cover_tilt_position', payload, (data) => {
+      if (data) {
+        callback();
+      } else {
+        callback(communicationError);
+      }
+    });
+  }
+}
+
 class HomeAssistantRollershutterBinary extends HomeAssistantRollershutter {
   transformData(data) {
-    return (data && data.state) ? ((data.state === 'open') * 100) : null;
+    return (data && data.state && data.state !== 'unavailable') ? ((data.state === 'open') * 100) : null;
   }
 
   setTargetState(position, callback, context) {
@@ -183,6 +232,8 @@ function HomeAssistantCoverFactory(log, data, client, firmware) {
 
   if (data.attributes.homebridge_cover_type === 'garage_door') {
     return new HomeAssistantGarageDoor(log, data, client, firmware);
+  } else if (data.attributes.homebridge_cover_type === 'horizontal_blind') {
+    return new HomeAssistantHorizontalBlind(log, data, client, firmware);
   } else if (data.attributes.homebridge_cover_type === 'rollershutter') {
     if (data.attributes.current_position !== undefined) {
       return new HomeAssistantRollershutter(log, data, client, firmware);
